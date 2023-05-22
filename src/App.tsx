d564564470a3b46fe6141d61a5762c76fa4ea72f
Akseli Palmer <akseli@toddsden.com>
2023-05-22 09:46:20 -0300

    added image and some basic text. Got stuck after that

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Typography, Card, Grid } from "@mui/joy";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid
+      container
+      sx={{
+        // height: "100vh",
+        // width: "100vw",
+        minWidth: "100vw",
+        minHeight: "100vh",
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1632516643720-e7f5d7d6ecc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2022&q=80)",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+        backgroundColor: "#464646",
+        justifyContent: "center",
+        // px: 10,
+        py: 3,
+      }}
+    >
+      <Grid
+        xs={10}
+        sm={12}
+        display="flex"
+        justifyContent="center"
+        alignItems="start"
+      >
+        <Card sx={{ maxWidth: 400 }}>
+          <Typography level="h4" mb={0.5} mt={0}>
+            Build Your Dream
+          </Typography>
+          <Typography>
+            Build Your Dream fast with the best tools on the market.
+          </Typography>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
